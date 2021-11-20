0b427fda8568d408f513eacb9453286e24c3e7a6
Glenn Pinto <gvpinto@gmail.com>
2021-11-20 13:18:58 -0500

    99. Binding to Directive Properties ex 2 - with default property binding []

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -13,7 +13,7 @@ import {
 })
 export class BetterHighlightDirective implements OnInit {
   @Input() defaultColor: String = 'transparent';
-  @Input() highlightColor: String = 'blue';
+  @Input('appBetterHighlight') highlightColor: String = 'blue';
   @HostBinding('style.backgroundColor') backgroundColor: String = 'transparent';
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
